03b1e1b0372a256ddcbf39198c0b696501e286d6
lei.guo <lei.guo@way.io>
2017-12-22 17:51:54 +0800

    commit

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 
-import com.example.demo.model.Book;
 import com.example.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
